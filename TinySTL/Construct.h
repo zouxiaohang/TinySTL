dfe5081a998d79426f2861fcce69e6a2d2fa151b
邹晓航 <1210603696@qq.com>
2014-09-17 09:20:34 +0800

    添加初始化和销毁对象的函数

---
+++ TinySTL/Construct.h
@@ -0,0 +1,38 @@
+#ifndef _CONSTRUCT_H_
+#define _CONSTRUCT_H_
+
+#include <new>
+
+#include "TypeTraits.h"
+
+namespace TinySTL{
+
+	template<class T1, class T2>
+	inline void construct(T1 *ptr1, const T2& value){
+		new(ptr1) T1(value);
+	}
+
+	template<class T>
+	inline void destroy(T *ptr){
+		ptr->~T();
+	}
+
+	template<class ForwardIterator>
+	inline void _destroy(ForwardIterator first, ForwardIterator last, _true_type){}
+
+	template<class ForwardIterator>
+	inline void _destroy(ForwardIterator first, ForwardIterator last, _false_type){
+		for (; , first != last; ++first){
+			destroy(&*first);
+		}
+	}
+
+	template<class ForwardIterator>
+	inline void destroy(ForwardIterator first, ForwardIterator last){
+		typedef typename _type_traits<T>::is_POD_type is_POD_type;
+		_destroy(first, last, is_POD_type);
+	}
+
+}
+
+#endif
--- TinySTL/main.cpp
+++ TinySTL/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "Construct.h"
+
 using namespace std;
 
 int main(){
