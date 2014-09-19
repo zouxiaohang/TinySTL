2fb1fbaf68df1bf6c86cbc22fb88682a25b33651
邹晓航 <1210603696@qq.com>
2014-09-19 12:29:38 +0800

    添加Iterator类型萃取功能

--- TinySTL/main.cpp
+++ TinySTL/main.cpp
@@ -3,16 +3,21 @@
 
 #include "Allocator.h"
 #include "Construct.h"
+#include "UninitializedFunctions.h"
 
 using namespace std;
 
 int main(){
-	for (int i = 1; i != 100000; ++i){
+	/*for (int i = 1; i != 100000; ++i){
 		auto p = TinySTL::allocator<int>::allocate();
-		TinySTL::allocator<int>::construct(p, i);
-		TinySTL::allocator<int>::destroy(p);
-		TinySTL::allocator<int>::deallocate(p);
-	}
+		
+	}*/
+	auto p = TinySTL::allocator<int>::allocate(100);
+	TinySTL::uninitialized_fill(p, p + 100, 88);
+	auto last = p + 100;
+	int array[100];
+	TinySTL::uninitialized_fill_n(array, 100, 88);
+	for (auto n : array){ cout << n << endl; }
 	system("pause");
 	return 0;
 }
