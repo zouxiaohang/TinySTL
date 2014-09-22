51df860f211d89fdffff1607e6a6d1c6722752e4
邹晓航 <1210603696@qq.com>
2014-09-22 13:07:14 +0800

    修复iterator_traits的一个bug

--- TinySTL/Iterator.h
+++ TinySTL/Iterator.h
@@ -64,11 +64,11 @@ namespace TinySTL{
 	template<class Iterator>
 	struct iterator_traits
 	{
-		typedef typename Iterator::Category		iterator_category;
-		typedef typename Iterator::value_type	value_type;
-		typedef typename Iterator::Distance		difference_type;
-		typedef typename Iterator::Pointer		pointer;
-		typedef typename Iterator::Reference 	reference;
+		typedef typename Iterator::iterator_category	iterator_category;
+		typedef typename Iterator::value_type			value_type;
+		typedef typename Iterator::difference_type		difference_type;
+		typedef typename Iterator::pointer				pointer;
+		typedef typename Iterator::reference 			reference;
 	};
 	template<class T>
 	struct iterator_traits<T*>
