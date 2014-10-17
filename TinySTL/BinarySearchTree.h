a0c0aaaa9e171b4108d238ab893acd8d7e35a320
邹晓航 <1210603696@qq.com>
2014-10-17 15:50:43 +0800

    添加二叉搜索树

---
+++ TinySTL/BinarySearchTree.h
@@ -0,0 +1,15 @@
+#ifndef _BINARY_SEARCH_TREE_H_
+#define _BINARY_SEARCH_TREE_H_
+
+#include "Allocator.h"
+
+namespace TinySTL{
+	//class of binary_search_tree
+	template<class T, class Alloc = TinySTL::allocator<T>>
+	class binary_search_tree{};
+	//class of bst iterator
+	template<class T>
+	class bst_iter{};
+}
+
+#endif
