107578b252952ead925e9bf59736854c5d6167ea
邹晓航 <1210603696@qq.com>
2014-10-24 16:08:12 +0800

    完成find_end

--- TinySTL/Algorithm.h
+++ TinySTL/Algorithm.h
@@ -235,6 +235,47 @@ namespace TinySTL{
 		}
 		return first;
 	}
+	//********** [find_end] ******************************
+	//********* [Algorithm Complexity: O(N*N)] ****************
+	template <class ForwardIterator1, class ForwardIterator2>
+	ForwardIterator1 find_end(ForwardIterator1 first1, ForwardIterator1 last1,
+		ForwardIterator2 first2, ForwardIterator2 last2){
+		if (first2 == last2)
+			return last1;
+		ForwardIterator1 ret = last1;
+		while (first1 != last1)
+		{
+			ForwardIterator1 it1 = first1;
+			ForwardIterator2 it2 = first2;
+			while (*it1 == *it2) {    
+				++it1; ++it2;
+				if (it2 == last2) { ret = first1; break; }
+				if (it1 == last1) return ret;
+			}
+			++first1;
+		}
+		return ret;
+	}
+	template <class ForwardIterator1, class ForwardIterator2, class BinaryPredicate>
+	ForwardIterator1 find_end(ForwardIterator1 first1, ForwardIterator1 last1,
+		ForwardIterator2 first2, ForwardIterator2 last2,
+		BinaryPredicate pred){
+		if (first2 == last2) 
+			return last1;  
+		ForwardIterator1 ret = last1;
+		while (first1 != last1)
+		{
+			ForwardIterator1 it1 = first1;
+			ForwardIterator2 it2 = first2;
+			while (pred(*it1, *it2)) {  
+				++it1; ++it2;
+				if (it2 == last2) { ret = first1; break; }
+				if (it1 == last1) return ret;
+			}
+			++first1;
+		}
+		return ret;
+	}
 }
 
 
