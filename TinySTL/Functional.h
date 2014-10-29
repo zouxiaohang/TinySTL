d97ee68238662d7ff4787252dd6cdc0902ec58cc
邹晓航 <1210603696@qq.com>
2014-10-29 15:01:28 +0800

    完成equal_to

--- TinySTL/Functional.h
+++ TinySTL/Functional.h
@@ -13,5 +13,16 @@ namespace TinySTL{
 			return x < y;
 		}
 	};
+	//********** [equal_to] ****************
+	template<class T>
+	struct equal_to{
+		typedef T first_argument_type;
+		typedef T second_argument_type;
+		typedef bool result_type;
+
+		result_type operator()(const first_argument_type& x, const second_argument_type& y){
+			return x == y;
+		}
+	};
 }
 #endif
