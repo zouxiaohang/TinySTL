7d8d72b8f249711c7bc9d1e6a32b816cefa526cd
邹晓航 <1210603696@qq.com>
2014-12-18 10:19:12 +0800

    添加测试辅助函数

---
+++ TinySTL/Test/TestUtil.h
@@ -0,0 +1,36 @@
+#ifndef _TEST_UTIL_H_
+#define _TEST_UTIL_H_
+
+#include <iterator>
+#include <iostream>
+
+#include "..\Algorithm.h"
+
+namespace TinySTL{
+	namespace Test{
+
+		template<class Container>
+		void print_container(Container& container){//不是每一个容器都有const_iterator
+			std::cout << "Container : ";
+			for (auto val : container){
+				std::cout << val << " ";
+			}
+			std::cout << std::endl;
+		}
+
+		template<class Container1, class Container2>
+		bool container_equal(Container1& con1, Container2& con2){//不是每一个容器都有const_iterator
+			/*return TinySTL::equal(std::begin(con1), std::end(con1),
+				std::begin(con2));*/
+			auto first1 = std::begin(con1), last1 = std::end(con1);
+			auto first2 = std::begin(con2), last2 = std::end(con2);
+			for (; first1 != last1 && first2 != last2; ++first1, ++first2){
+				if (*first1 != *first2)
+					return false;
+			}
+			return (first1 == last1 && first2 == last2);
+		}
+	}
+}
+
+#endif
