310e157beafe141d877755af858771012c5e2f55
邹晓航 <1210603696@qq.com>
2014-12-20 10:52:01 +0800

    add

---
+++ TinySTL/Test/AlgorithmTest.h
@@ -0,0 +1,18 @@
+#ifndef _ALGORITHM_TEST_H_
+#define _ALGORITHM_TEST_H_
+
+#include "TestUtil.h"
+
+#include "../Algorithm.h"
+#include <algorithm>
+
+#include <cassert>
+#include <vector>
+
+namespace TinySTL{
+	namespace AlgorithmTest{
+		void testFIll();
+	}
+}
+
+#endif
