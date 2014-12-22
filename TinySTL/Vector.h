163f771a55d5dcfd97d18c25290647a6a76a42a0
邹晓航 <1210603696@qq.com>
2014-12-22 09:33:41 +0800

    bug fix

--- TinySTL/Vector.h
+++ TinySTL/Vector.h
@@ -23,7 +23,8 @@ namespace TinySTL{
 	public:
 		typedef T									value_type;
 		typedef T*							iterator;
-		typedef const iterator					const_iterator;
+		//typedef const iterator					const_iterator;
+		typedef const T*							const_iterator;
 		typedef reverse_iterator_t<T*>				reverse_iterator;
 		typedef reverse_iterator_t<const T*>				const_reverse_iterator;
 		typedef iterator							pointer;
