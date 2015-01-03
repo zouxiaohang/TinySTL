4e48d0061e786b047e8b9b2fd282377da88df1f0
邹晓航 <1210603696@qq.com>
2015-01-03 12:03:38 +0800

    完成stack的测试

---
+++ TinySTL/Test/StackTest.h
@@ -0,0 +1,26 @@
+#ifndef _STACK_TEST_H_
+#define _STACK_TEST_H_
+
+#include "TestUtil.h"
+
+#include "../Stack.h"
+#include <stack>
+
+#include <cassert>
+#include <string>
+
+namespace TinySTL{
+	namespace StackTest{
+		template<class T>
+		using stdSt = std::stack < T > ;
+		template<class T>
+		using tsSt = TinySTL::stack < T > ;
+
+		void testCase1();
+		void testCase2();
+		void testCase3();
+		void testCase4();
+	}
+}
+
+#endif
