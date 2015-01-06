88c92bde97e75e77f53857941b59f5fb2cb80ceb
邹晓航 <1210603696@qq.com>
2015-01-06 13:32:40 +0800

    update

--- TinySTL/Test/CircularBufferTest.cpp
+++ TinySTL/Test/CircularBufferTest.cpp
@@ -1,85 +1,85 @@
-#include "CircularBufferTest.h"
-
-namespace TinySTL{
-	namespace CircularBufferTest{
-		void testCase1(){
-			tsCB<int, 10> cb1(10, 1);
-			for (auto i = 0; i != 10; ++i)
-				assert(cb1[i] == 1);
-
-			int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-			tsCB<int, 10> cb2(std::begin(arr), std::end(arr));
-			for (auto i = 0; i != 10; ++i)
-				assert(cb2[i] == i);
-
-			auto cb3(cb2);
-			assert(circular_buffer_equal(cb2, cb3));
-
-			auto cb4(std::move(cb2));//cb2 clear
-			assert(circular_buffer_equal(cb3, cb4));
-
-			auto cb5 = cb3;
-			assert(circular_buffer_equal(cb3, cb5));
-
-			auto cb6 = std::move(cb3);//cb3 clear
-			assert(circular_buffer_equal(cb5, cb6));
-		}
-		void testCase2(){
-			tsCB<int, 2> cb(1);
-			assert(cb.size() == 1);
-			cb.pop_front();
-			assert(!cb.full());
-			assert(cb.size() == 0);
-			assert(cb.empty());
-
-			cb.push_back(1), cb.push_back(2);
-			assert(cb.full());
-			assert(!cb.empty());
-			assert(cb.size() == 2);
-		}
-		void testCase3(){
-			tsCB<std::string, 3> cb(3);
-			cb[0] = "one", cb[1] = "two", cb[2] = "three";
-
-			assert(*(cb.first()) == "one" && *(cb.last()) == "three");
-
-			assert(cb.front() == "one" && cb.back() == "three");
-		}
-		void testCase4(){
-			tsCB<std::string, 3> cb(1);
-			assert(cb.front() == std::string());
-
-			cb.push_back("zxh"); cb.push_back("jwl");
-			assert(cb.back() == "jwl");
-			cb.pop_front();
-			assert(cb.front() == "zxh");
-		}
-		void testCase5(){
-			tsCB<int, 3> cb1(3), cb2(3);
-			assert(cb1 == cb2);
-			assert(!(cb1 != cb2));
-
-			cb1[0] = -1;
-			assert(!(cb1 == cb2));
-			assert(cb1 != cb2);
-		}
-		void testCase6(){
-			std::string arr[] = { "1", "2", "3" };
-			tsCB<std::string, 3> cb(std::begin(arr), std::end(arr));
-			std::cout << cb << std::endl;
-		}
-	}
-}
-
-//using namespace TinySTL::CircularBufferTest;
-//int main(){
-//	testCase1();
-//	testCase2();
-//	testCase3();
-//	testCase4();
-//	testCase5();
-//	testCase6();
+//#include "CircularBufferTest.h"
 //
-//	system("pause");
-//	return 0;
+//namespace TinySTL{
+//	namespace CircularBufferTest{
+//		void testCase1(){
+//			tsCB<int, 10> cb1(10, 1);
+//			for (auto i = 0; i != 10; ++i)
+//				assert(cb1[i] == 1);
+//
+//			int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//			tsCB<int, 10> cb2(std::begin(arr), std::end(arr));
+//			for (auto i = 0; i != 10; ++i)
+//				assert(cb2[i] == i);
+//
+//			auto cb3(cb2);
+//			assert(circular_buffer_equal(cb2, cb3));
+//
+//			auto cb4(std::move(cb2));//cb2 clear
+//			assert(circular_buffer_equal(cb3, cb4));
+//
+//			auto cb5 = cb3;
+//			assert(circular_buffer_equal(cb3, cb5));
+//
+//			auto cb6 = std::move(cb3);//cb3 clear
+//			assert(circular_buffer_equal(cb5, cb6));
+//		}
+//		void testCase2(){
+//			tsCB<int, 2> cb(1);
+//			assert(cb.size() == 1);
+//			cb.pop_front();
+//			assert(!cb.full());
+//			assert(cb.size() == 0);
+//			assert(cb.empty());
+//
+//			cb.push_back(1), cb.push_back(2);
+//			assert(cb.full());
+//			assert(!cb.empty());
+//			assert(cb.size() == 2);
+//		}
+//		void testCase3(){
+//			tsCB<std::string, 3> cb(3);
+//			cb[0] = "one", cb[1] = "two", cb[2] = "three";
+//
+//			assert(*(cb.first()) == "one" && *(cb.last()) == "three");
+//
+//			assert(cb.front() == "one" && cb.back() == "three");
+//		}
+//		void testCase4(){
+//			tsCB<std::string, 3> cb(1);
+//			assert(cb.front() == std::string());
+//
+//			cb.push_back("zxh"); cb.push_back("jwl");
+//			assert(cb.back() == "jwl");
+//			cb.pop_front();
+//			assert(cb.front() == "zxh");
+//		}
+//		void testCase5(){
+//			tsCB<int, 3> cb1(3), cb2(3);
+//			assert(cb1 == cb2);
+//			assert(!(cb1 != cb2));
+//
+//			cb1[0] = -1;
+//			assert(!(cb1 == cb2));
+//			assert(cb1 != cb2);
+//		}
+//		void testCase6(){
+//			std::string arr[] = { "1", "2", "3" };
+//			tsCB<std::string, 3> cb(std::begin(arr), std::end(arr));
+//			std::cout << cb << std::endl;
+//		}
+//	}
 //}
+//
+////using namespace TinySTL::CircularBufferTest;
+////int main(){
+////	testCase1();
+////	testCase2();
+////	testCase3();
+////	testCase4();
+////	testCase5();
+////	testCase6();
+////
+////	system("pause");
+////	return 0;
+////}
--- TinySTL/Test/SuffixArrayTest.cpp
+++ TinySTL/Test/SuffixArrayTest.cpp
@@ -1,32 +1,32 @@
-#include "SuffixArrayTest.h"
-
-namespace TinySTL{
-	namespace SuffixArrayTest{
-		void testCase(){
-			//char arr[] = { 'a', 'a', 'b', 'a', 'a', 'a', 'a', 'b' };
-			std::string str("aabaaaab");
-
-			//TinySTL::suffix_array sa(arr, 8);
-			TinySTL::suffix_array sa(str.data(), str.size());
-			auto sa1 = sa.suffixArray();
-			auto sa2 = TinySTL::suffix_array::array_type{3, 4, 5, 0, 6, 1, 7, 2};
-			assert(TinySTL::Test::container_equal(sa1, sa2));
-
-			auto ra1 = sa.rankArray();
-			auto ra2 = TinySTL::suffix_array::array_type{ 3, 5, 7, 0, 1, 2, 4, 6 };
-			assert(TinySTL::Test::container_equal(ra1, ra2));
-
-			auto ha1 = sa.heightArray();
-			auto ha2 = TinySTL::suffix_array::array_type{ 3, 2, 3, 1, 2, 0, 1 };
-			assert(TinySTL::Test::container_equal(ha1, ha2));
-		}
-	}
-}
-
-//using namespace TinySTL::SuffixArrayTest;
-//int main(){
-//	testCase();
+//#include "SuffixArrayTest.h"
 //
-//	system("pause");
-//	return 0;
+//namespace TinySTL{
+//	namespace SuffixArrayTest{
+//		void testCase(){
+//			//char arr[] = { 'a', 'a', 'b', 'a', 'a', 'a', 'a', 'b' };
+//			std::string str("aabaaaab");
+//
+//			//TinySTL::suffix_array sa(arr, 8);
+//			TinySTL::suffix_array sa(str.data(), str.size());
+//			auto sa1 = sa.suffixArray();
+//			auto sa2 = TinySTL::suffix_array::array_type{3, 4, 5, 0, 6, 1, 7, 2};
+//			assert(TinySTL::Test::container_equal(sa1, sa2));
+//
+//			auto ra1 = sa.rankArray();
+//			auto ra2 = TinySTL::suffix_array::array_type{ 3, 5, 7, 0, 1, 2, 4, 6 };
+//			assert(TinySTL::Test::container_equal(ra1, ra2));
+//
+//			auto ha1 = sa.heightArray();
+//			auto ha2 = TinySTL::suffix_array::array_type{ 3, 2, 3, 1, 2, 0, 1 };
+//			assert(TinySTL::Test::container_equal(ha1, ha2));
+//		}
+//	}
 //}
+//
+////using namespace TinySTL::SuffixArrayTest;
+////int main(){
+////	testCase();
+////
+////	system("pause");
+////	return 0;
+////}
