8b27ad2d9e90980b63bc262168fb93849e380ff1
邹晓航 <1210603696@qq.com>
2015-01-06 13:20:23 +0800

    删除多余的头文件

--- TinySTL/Test/TestUtil.h
+++ TinySTL/Test/TestUtil.h
@@ -5,8 +5,6 @@
 #include <iostream>
 #include <string>
 
-#include "..\Algorithm.h"
-
 namespace TinySTL{
 	namespace Test{
 
@@ -21,8 +19,6 @@ namespace TinySTL{
 
 		template<class Container1, class Container2>
 		bool container_equal(Container1& con1, Container2& con2){//不是每一个容器都有const_iterator
-			/*return TinySTL::equal(std::begin(con1), std::end(con1),
-				std::begin(con2));*/
 			auto first1 = std::begin(con1), last1 = std::end(con1);
 			auto first2 = std::begin(con2), last2 = std::end(con2);
 			for (; first1 != last1 && first2 != last2; ++first1, ++first2){
