4dd2dfe8386fe4f0566b0fef41f8418dd213e616
邹晓航 <1210603696@qq.com>
2015-02-11 10:45:21 +0800

    完成test

---
+++ TinySTL/Test/GraphTest.h
@@ -0,0 +1,22 @@
+#ifndef _GRAPH_TEST_H_
+#define _GRAPH_TEST_H_
+
+#include "TestUtil.h"
+
+#include "../Graph.h"
+
+#include <cassert>
+
+namespace TinySTL{
+	namespace GraphTest{
+		template<class Index, class Value>
+		using dGraph = TinySTL::directed_graph < Index, Value > ;
+
+		void testCase1();
+		void testCase2();
+
+		void testAllCases();
+	}
+}
+
+#endif
